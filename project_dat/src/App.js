2d2cfd29daaa455f890b83fd39bed27bfcea6192
dattanlee@gmail.com <dattanlee@gmail.com>
2025-02-28 19:07:39 +0700

    creat project reactjs

---
+++ project_dat/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './layout/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>Chào bạn đến với ứng dụng ReactJS đầu tiên!</p>
+    </div>
+  );
+}
+
+export default App;
